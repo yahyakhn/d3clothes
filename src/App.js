c96757683d64ba1fa6689a7570c49d3c69edee0f
Yahya Khan <90936613+yahyakhn@users.noreply.github.com>
2023-04-20 03:47:22 +0530

    initializing repo

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
+
+// Components
+import Navigation from './components/Navigation'
+import Section from './components/Section'
+import Product from './components/Product'
+
+// ABIs
+import d3clothes from './abis/d3clothes.json'
+
+// Config
+import config from './config.json'
+
+function App() {
+
+  return (
+    <div>
+
+      <h2>Welcome to d3clothes</h2>
+
+    </div>
+  );
+}
+
+export default App;
