2e9cb3d590447c98eef3b0e810a200d4fcc83291
Yahya Khan <90936613+yahyakhn@users.noreply.github.com>
2023-06-11 18:37:33 +0530

    adding object feilds in frontend

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import config from './config.json'
 
 function App() {
 const [provider, setProvider] = useState(null)
+const [D3clothes, setd3clothes] = useState(null)
+
 const [account, setAccount] = useState(null) //this makes the site stateful (remembers the previous state)
 const loadblockchainData = async() => {
   //connect to blockchain(we have to use ethers provider)
@@ -24,9 +26,20 @@ const loadblockchainData = async() => {
   const network = await provider.getNetwork()
   console.log(network)
   //connect to smart contracts(load js versions)
-  
-
+  const D3clothes = new ethers.Contract(
+    config[network.chainId].d3clothes.address, d3clothes , provider)//abi = abstract binary interface
+    setd3clothes(D3clothes)
   //load blockchain data
+  const items = []
+  for (var i = 0; i < 9; i++){
+    const item = await D3clothes.items(i + 1)
+    items.push(item)
+  }
+    const clothing = items.filter((item) => item.category === "clothing")
+    const toys = items.filter((item) => item.category === "toys")
+    const electronics = items.filter((item) => item.category === "electronics")
+    const jewelry = items.filter((item) => item.category === jewelry")
+
 }
 useEffect(()=> {
   loadblockchainData()
