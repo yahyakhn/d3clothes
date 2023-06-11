b83f331097a0a70374350c884116185b94b0a70c
Yahya Khan <90936613+yahyakhn@users.noreply.github.com>
2023-06-11 17:04:06 +0530

    front end navbar and other details created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react' // importing libraries, useeffect is a hook from react which let us call a function whenever a function is rendered
 import { ethers } from 'ethers'
 
 // Components
@@ -13,11 +13,32 @@ import d3clothes from './abis/d3clothes.json'
 import config from './config.json'
 
 function App() {
+const [provider, setProvider] = useState(null)
+const [account, setAccount] = useState(null) //this makes the site stateful (remembers the previous state)
+const loadblockchainData = async() => {
+  //connect to blockchain(we have to use ethers provider)
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  setProvider(provider) 
+
+
+  const network = await provider.getNetwork()
+  console.log(network)
+  //connect to smart contracts(load js versions)
+  
+
+  //load blockchain data
+}
+useEffect(()=> {
+  loadblockchainData()
+},[])//[] is empty string
 
   return (
     <div>
+      <Navigation account={account} setAccount={setAccount} ></Navigation>
 
-      <h2>Welcome to d3clothes</h2>
+      <h2>our best sellers</h2>
+      {/* <p>{account}</p>
+       */}
 
     </div>
   );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,26 @@
 import { ethers } from 'ethers';
 
 const Navigation = ({ account, setAccount }) => {
+    const connectHandler = async () => {
+        const accounts = await window.ethereum.request({method: 'eth_requestAccounts' });
+        const account = ethers.utils.getAddress(accounts[0])
+        setAccount(account)
+    }
 
     return (
-        <nav>
-
+        <nav> 
+            <div className='nav__brand'><h1>D3clothes</h1>
+             </div>
+             <input type= 'text' className='nav__search'></input>
+             {account? (
+             <button type= 'button' className='nav__connect'>
+             {account.slice(0, 6)+ '...'+account.slice(38, 42)}</button>):(
+             <button type='button' className='nav__connect' onClick={connectHandler}>connect</button>)}
+             <ul className='nav__links'>
+                <li><a href='#clothing'>Clothing</a></li>
+                <li><a href='#jewelry and accesories'>jewelry and accesories</a></li>
+                <li><a href='home decor'>home decor</a></li>
+             </ul>
         </nav>
     );
 }
