f21f4a1f1c60375769a665b6bf5b886a41c21cec
Yahya Khan <90936613+yahyakhn@users.noreply.github.com>
2023-06-12 19:42:50 +0530

    created sections added images and information to the frontend

--- src/App.js
+++ src/App.js
@@ -11,36 +11,48 @@ import d3clothes from './abis/d3clothes.json'
 
 // Config
 import config from './config.json'
+import { isCompositeComponentWithType } from 'react-dom/test-utils'
 
 function App() {
+const [account, setAccount] = useState(null) //this makes the site stateful (remembers the previous state)
 const [provider, setProvider] = useState(null)
 const [D3clothes, setd3clothes] = useState(null)
 
-const [account, setAccount] = useState(null) //this makes the site stateful (remembers the previous state)
-const loadblockchainData = async() => {
-  //connect to blockchain(we have to use ethers provider)
-  const provider = new ethers.providers.Web3Provider(window.ethereum)
-  setProvider(provider) 
 
+const [electronics, setElectonics] = useState(null)
+const [clothing, setClothing] = useState(null)
+const [toys, setToys] = useState(null)
 
-  const network = await provider.getNetwork()
-  console.log(network)
-  //connect to smart contracts(load js versions)
-  const D3clothes = new ethers.Contract(
-    config[network.chainId].d3clothes.address, d3clothes , provider)//abi = abstract binary interface
-    setd3clothes(D3clothes)
-  //load blockchain data
-  const items = []
-  for (var i = 0; i < 9; i++){
-    const item = await D3clothes.items(i + 1)
-    items.push(item)
-  }
-    const clothing = items.filter((item) => item.category === "clothing")
-    const toys = items.filter((item) => item.category === "toys")
-    const electronics = items.filter((item) => item.category === "electronics")
-    const jewelry = items.filter((item) => item.category === "jewelry")
-
+const togglePop = () =>{
+  console.log ("togglepop...")
 }
+
+  async function loadblockchainData() {
+    //connect to blockchain(we have to use ethers provider)
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+
+    const network = await provider.getNetwork()
+    console.log(network)
+
+    //connect to the smart contract (create JS version)
+    const D3clothes = new ethers.Contract(config[network.chainId].D3clothes.address, d3clothes, provider)
+    setd3clothes(D3clothes)
+  //load products
+    const items = []
+    for (var i = 0 ; i < 9; i++){
+      const item = await D3clothes.items(i + 1)
+      items.push(item)
+    }
+     const electronics = items.filter((item) => item.category === 'electronics') 
+     const toys = items.filter((item) => item.category === 'toys') 
+     const clothing = items.filter((item) => item.category === 'clothing') 
+
+     setClothing(clothing)
+     setElectonics(electronics)
+     setToys(toys)
+  }
+
 useEffect(()=> {
   loadblockchainData()
 },[])//[] is empty string
@@ -50,6 +62,13 @@ useEffect(()=> {
       <Navigation account={account} setAccount={setAccount} ></Navigation>
 
       <h2>our best sellers</h2>
+      {electronics && clothing && toys &&(
+        <>
+        <Section title ={"clothing & Jewelry"} items={clothing} togglePop={togglePop}/>
+        <Section title ={"electronics"} items={electronics} togglePop={togglePop}/>
+        <Section title ={"Toys"} items={toys} togglePop={togglePop}/>
+        </>
+      )}
       {/* <p>{account}</p>
        */}
 
